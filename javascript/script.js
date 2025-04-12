199ff2fd6c168eabfda630b75b626b93c59b6429
Natiele <natiele887@gmail.com>
2025-04-12 18:18:39 -0300

    Fazendo parte resposividade no javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-    // Parte do botão mobile
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
@@ -11,26 +9,34 @@ $(document).ready(function () {
 
     $(window).on('scroll', function () {
         const header = $('header');
-        const scrollPossition = $(window).scrollTop() - header.outerHeight();
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
 
-        if (scrollPossition > 0) {
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
-            header.css('box-shadow', '0px 0px 10px 2px rgb(0,0,0,0.1)');
+            header.css('box-shadow', '0 2px 4px rgba(0, 0, 0, 0.1)');
         }
 
-        sections.each(function (i) {
+        sections.each(function(i) {
             const section = $(this);
-            const sectionTop = section.offset().top - header.outerHeight();
+            const sectionTop = section.offset().top - 96;
             const sectionBottom = sectionTop + section.outerHeight();
 
-            if (scrollPossition >= sectionTop && scrollPossition <= sectionBottom) {
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
                 return false;
             }
         });
 
         navItems.removeClass('active');
-        navItems.eq(activeSectionIndex).addClass('active');
-        });
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
 });
