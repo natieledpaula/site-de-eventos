0fa67f2ce2512fc19c2a4943530987dfa228f93c
GabrielVanco <gabrielvanco111223@gmail.com>
2025-04-20 15:54:13 -0300

    Inserindo a section de inscrição para o evento e estilizando com css adaptando para a tela

--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,42 @@
 $(document).ready(function () {
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+  $("#mobile_btn").on("click", function () {
+    $("#mobile_menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
+  });
+
+  const sections = $("section");
+  const navItems = $(".nav_item");
+
+  $(window).on("scroll", function () {
+    const header = $("header");
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    let activeSectionIndex = 0;
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "0 2px 4px rgba(0, 0, 0, 0.1)");
+    }
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - header.outerHeight();
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false;
+      }
     });
 
-    const sections = $('section');
-    const navItems = $('.nav_item');
+    navItems.removeClass("active");
+    $(navItems[activeSectionIndex]).addClass("active");
+  });
 
-    $(window).on('scroll', function () {
-        const header = $('header');
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
-        let activeSectionIndex = 0;
-
-        if (scrollPosition <= 0) {
-            header.css('box-shadow', 'none');
-        } else {
-            header.css('box-shadow', '0 2px 4px rgba(0, 0, 0, 0.1)');
-        }
-
-        sections.each(function(i) {
-            const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop + section.outerHeight();
-
-            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeSectionIndex = i;
-                return false;
-            }
-        });
-
-        navItems.removeClass('active');
-        $(navItems[activeSectionIndex]).addClass('active');
-    });
-
-    ScrollReveal().reveal('#cta', {
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
 });
