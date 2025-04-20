1e4c081204b3487ff5cae9c6167dbc95abfcb906
GabrielVanco <gabrielvanco111223@gmail.com>
2025-04-20 16:15:05 -0300

    Criando arquivo de inscrição em javaScript para validar e evitar campos vázios

---
+++ javascript/inscricao.js
@@ -0,0 +1,32 @@
+document
+  .getElementById("formulario")
+  .addEventListener("submit", validarFormulario);
+
+function validarFormulario(event) {
+  event.preventDefault();
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const termos = document.getElementById("termos").checked;
+
+  if (!nome || !email || !telefone) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  if (!termos) {
+    alert("Por favor, aceite os termos e condições.");
+    return;
+  }
+
+  const dados = { nome, email, telefone };
+
+  localStorage.setItem("cadastroEvento", JSON.stringify(dados));
+
+  alert("Cadastro realizado com sucesso!");
+  window.location.href = "confirmarCadastro.html";
+
+  document.querySelector("form").reset();
+
+  console.log(dados);
+}
